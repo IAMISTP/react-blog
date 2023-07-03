5e6002935957ab5f3725b83fe71fdf8d8037297e
박혜민 <hyeminpak@gmail.com>
2023-07-04 03:10:53 +0900

    가나다라순서 변경 버튼 기능구현

--- src/App.js
+++ src/App.js
@@ -13,11 +13,22 @@ function App() {
   function goodEvent() {
     setGoodCount(goodCount + 1);
   }
+  function changeTitle() {
+    let copyTitle = [...title];
+    copyTitle[0] = "여자코트추천";
+    setTitle(copyTitle);
+  }
+  function changeOrder() {
+    let changeTitle = [...title];
+    setTitle(changeTitle.sort());
+  }
   return (
     <div className="App">
       <div className="black-nav">
         <h4>reactBlog</h4>
       </div>
+      <button onClick={changeOrder}>가나라다순으로 변경</button>
+      <button onClick={changeTitle}>글제목 변경</button>
       <div className="list">
         <h4>
           {title[0]}

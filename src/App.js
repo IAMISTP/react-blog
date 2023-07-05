d72fe83071bd7583d2eb6f63ba306766f19145e3
박혜민 <hyeminpak@gmail.com>
2023-07-05 16:55:55 +0900

    props 이용해서 제목변경,모달배경색 변경 기능구현

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
 
-function Modal() {
+function Modal(props) {
   return (
-    <div className="modal">
-      <h4>제목</h4>
+    <div className="modal" style={{ backgroundColor: props.color }}>
+      <h4>{props.titles[0]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button onClick={props.changeTitle}>글수정</button>
     </div>
   );
 }
 
 function App() {
-  let [title, setTitle] = useState([
+  let [titles, setTitles] = useState([
     "남자코트추천",
     "강남우동맛집",
     "파이썬독학",
@@ -23,12 +24,12 @@ function App() {
   let [modal, setModal] = useState(false);
 
   function changeTitle() {
-    let copyTitle = [...title];
+    let copyTitle = [...titles];
     copyTitle[0] = "여자코트추천";
-    setTitle(copyTitle);
+    setTitles(copyTitle);
   }
   function changeOrder() {
-    let changeTitle = [...title];
+    let changeTitle = [...titles];
     setTitle(changeTitle.sort());
   }
   return (
@@ -39,7 +40,7 @@ function App() {
       <button onClick={changeOrder}>가나라다순으로 변경</button>
       <button onClick={changeTitle}>글제목 변경</button>
 
-      {title.map((x, i) => {
+      {titles.map((x, i) => {
         return (
           <div className="list" key={i}>
             <h4 onClick={() => setModal(!modal)}>
@@ -58,7 +59,9 @@ function App() {
           </div>
         );
       })}
-      {modal ? <Modal /> : null}
+      {modal ? (
+        <Modal titles={titles} color={"skyblue"} changeTitle={changeTitle} />
+      ) : null}
     </div>
   );
 }
